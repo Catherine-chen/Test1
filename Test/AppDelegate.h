8ad19b98145479b24064c4e1df238f8e840ce1c5
jiangchenyi <jiangchenyi@jiangchenyideMacBook-Pro.local>
2015-06-22 09:50:01 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by jiangchenyi on 15/6/22.
+//  Copyright (c) 2015年 jiangchenyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by jiangchenyi on 15/6/22.
+//  Copyright (c) 2015年 jiangchenyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

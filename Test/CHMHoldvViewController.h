d51ef6853e315693a6d1d36f7c42d1b23abd27f1
Catherine-chen <jiangchenyi@jiangchenyideMacBook-Pro.local>
2015-07-16 17:27:46 +0800

    add hold view controller

---
+++ Test/CHMHoldvViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHMHoldvViewController.h
+//  Test
+//
+//  Created by jiangchenyi on 15/7/16.
+//  Copyright (c) 2015年 jiangchenyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHMHoldvViewController : UIViewController
+
+@end
